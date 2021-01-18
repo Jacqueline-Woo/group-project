d78f9c30c1ba8289c9820a1bdffc2a1e7169a62e
Katy <yuzhu.chen05@gmail.com>
2021-01-19 03:59:18 +0800

    New one combined

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 import csv
+import os
 
 app = Flask(__name__)
+
+picFolder = os.path.join('static','pics')
+app.config['UPLOAD_FOLDER'] = picFolder
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
+# HTML page?
 @app.route('/about')
-def about():
-    return 'This site was developed by Arpita'
+def about_us():
+   return render_template("about_us.html")
+
+# HTML page?
+@app.route('/profile')
+def profile():
+   return render_template("profile.html")
 
 @app.route('/hello/<uname>')
 def say_hello(uname):
@@ -27,7 +37,10 @@ def return_users():
                 users.append({
                 "fname": row[0],
                 "lname": row[1],
-                "city": row[2]
+                "city": row[2],
+                "email": row[3],
+                "password": row[4],
+                "confirmpassword": row[5]
                 })
             else:
                 first_line = False
@@ -48,40 +61,59 @@ def submit_form():
         fname = userdata["fname"]
         lname = userdata["lname"]
         city = userdata["city"]
-        if( len(fname) < 1 or len(lname) < 1 or len(city) < 1 ):
+        email = userdata["email"]
+        password = userdata["password"]
+        confirmpassword = userdata["confirmpassword"]
+        if( len(fname) < 1 or len(lname) < 1 or len(city) < 1 or len(email) < 1 or len(password) < 1  or len(confirmpassword) < 1 ):
             return render_template("new_user.html", status='Please resubmit with valid information.')
         else:
             with open('data/users.csv', mode='a', newline='') as file:
                 data = csv.writer(file)
-                data.writerow([fname, lname, city])
-            return render_template("new_user.html", status='User added!')
+                data.writerow([fname, lname, city, email, password, confirmpassword])
+            return render_template("feed.html", status='You have successfully created an account!') 
+            
 
 # HTML form
-@app.route('/getUser')
-def get_user():
-    return render_template("get_user.html")
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
-@app.route('/getUser', methods=["POST"])
-def return_users_by_city():
+@app.route('/login', methods=["POST"])
+def users_login_email_password():
     userdata = dict(request.form)
-    city = userdata["city"]
-    if( len(city) < 1 ):
-        return render_template("get_user.html", status="Invalid City")
+    email = userdata["email"]
+    password = userdata["password"]
+    if( len(password)<10):
+        return render_template("login.html", status="Invalid password, please enter again.")
     with open('data/users.csv') as file:
         data = csv.reader(file, delimiter=',')
         first_line = True
         users = []
         for row in data:
             if not first_line:
-                if( row[2].strip() == city.strip() ):
-                    users.append({
-                    "fname": row[0],
-                    "lname": row[1]
-                    })
+                if( row[3].strip() == email.strip() and row[4].strip() == password.strip() ):
+                    return render_template("feed.html", status="Users found!",users = users)
             else:
                 first_line = False
-    if( len(users) == 0 ):
-        status = "No Users Found for specified city."
-    else:
-        status = "Users found!"
-    return render_template("get_user.html", status=status, users=users)
+
+    if not ( row[3].strip() == email.strip() and row[4].strip() == password.strip() ):
+            return render_template("new_user.html",status = "Don't have accoun't? Regiter now!")
+
+@app.route('/feed')
+def feed():
+    return render_template("feed.html")
+
+@app.route('/feed', methods=["GET", "POST"])
+def submit_post():
+    if request.method == "GET":
+        return redirect(url_for('feed'))
+    elif request.method == "POST":
+        submission = dict(request.form)
+        post = submission["post"]
+        if( len(post) < 1 ):
+            return render_template("feed.html", status='Submission was blank. Please try again.')
+        else:
+            with open('data/posts.csv', mode='a', newline='') as file:
+                data = csv.writer(file)
+                data.writerow([post])
+            return render_template("feed.html", status='Post submitted!')
